5affd6ca6f200a17b1c7f288d0f4253a62674f4f
Gustavo <140539608+Luxipe@users.noreply.github.com>
2024-01-29 17:09:44 -0300

    Add files via upload

---
+++ pudim2.js
@@ -0,0 +1,6 @@
+const minhaFuncao = (array) => array.map((element) => element * 2);
+
+// Exemplo de uso
+const meuArray = [1, 2, 3, 4];
+const resultado = minhaFuncao(meuArray);
+console.log(resultado); // Saída esperada: [2, 4, 6, 8]
